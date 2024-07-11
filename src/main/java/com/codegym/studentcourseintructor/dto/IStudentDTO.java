f402f41b067b84586c55f7919beefb451fd385b5
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-12 04:10:50 +0700

    update databases

---
+++ src/main/java/com/codegym/studentcourseintructor/dto/IStudentDTO.java
@@ -0,0 +1,12 @@
+package com.codegym.studentcourseintructor.dto;
+
+public interface IStudentDTO {
+    String getStudentName();
+
+    String getStudentEmail();
+
+    String getCourseTitle();
+
+    String getInstructorName();
+
+}
